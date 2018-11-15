f9cfd9937ef41865ec96e2e70c37eed0b48412df
hua0122 <1835591342@qq.com>
2018-11-15 11:10:23 +0800

    html

--- application/wap/controller/User.php
+++ application/wap/controller/User.php
@@ -90,49 +90,43 @@ class User extends Fornt
     //获取微信用户信息
     public function getwxinfo()
     {
+
         if (!empty($_REQUEST['data']) || !empty(session("openid"))) {
             $data = $_REQUEST['data'];
-
+            //echo $data;
             $data = json_decode($data, JSON_UNESCAPED_UNICODE);
             $data = json_decode($data);
-            var_dump($data);
-            if($data!=null){
-                    if (session("openid")) {
-                        session("openid", session("openid"));
-                    } else {
-                        session("openid", $data->openid);//这一步保存openid到session
-                    }
 
-                    $info = model('WxUser')->where(array("openid" => $data->openid))->find();
-
-                    if (count($info) <= 0) {
-                        $sign = array(
-                            "openid" => $data->openid,
-                            "nickname" => $data->nickname,
-                            'sex' => $data->sex,
-                            "city" => $data->city,
-                            "country" => $data->country,
-                            "province" => $data->province,
-                            "headimgurl" => $data->headimgurl,
-                            "subscribe_time" => time()
-                        );
-
-                        model('WxUser')->save($sign);
-                    }
-
-                    $url = "index";
-                    header("Location:" . $url);
-                    exit();
-                } else {
-                    echo "请在微信客户端打开";
-
-                }
+            if (session("openid")) {
+                session("openid", session("openid"));
+            } else {
+                session("openid", $data->openid);//这一步保存openid到session
             }
+            $info = model('WxUser')->where(array("openid" => $data->openid))->find();
+
+            if (count($info) <= 0) {
+                $sign = array(
+                    "openid"  => $data->openid,
+                    "nickname"  => $data->nickname,
+                    'sex' => $data->sex,
+                    "city"  => $data->city,
+                    "country"  => $data->country,
+                    "province"  => $data->province,
+                    "headimgurl"  => $data->headimgurl,
+                    "subscribe_time" => time()
+                );
+
+                model('WxUser')->save($sign);
+            }
+
+            $url = "../wap";
+            header("Location:" . $url);
+            exit();
+        }
 
         $url = "/l_wx/getwxinfo.php?method=getUserInfo&state=djjx";
         header("Location:" . $url);
         exit();
-
     }
 
 
