14d53a5a08d630c9782c290a92d49ef68c55728c
hua0122 <1835591342@qq.com>
2018-12-12 18:55:24 +0800

    html

--- application/api/controller/Protect.php
+++ application/api/controller/Protect.php
@@ -620,15 +620,20 @@ class Protect extends Api
                     }
 
                     $list[$k]['time1'] = $v['time'];
-                    if($k1!="time1"){
-                        $list = array_values($list[$k]);
-                    }
 
                 }
 
             }
 
             $list = array_values($list);
+
+            foreach ($list as $k=>$v){
+                foreach ($v as $k1=>$v1){
+                    if($k1!="time1"){
+                        $list = array_values($list[$k1]);
+                    }
+                }
+            }
 
 
 
