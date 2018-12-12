8e8bf883ab5955ed33bf78f080c9cdc3ead90810
hua0122 <1835591342@qq.com>
2018-12-12 18:36:46 +0800

    html

--- application/api/controller/Protect.php
+++ application/api/controller/Protect.php
@@ -618,6 +618,7 @@ class Protect extends Api
                         $list[$k][$v1['person']]['person'] = $this->get_name($v1['person']);
 
                     }
+                    $list = array_values($list[$k]);
                     $list[$k]['time1'] = $v['time'];
 
                 }
