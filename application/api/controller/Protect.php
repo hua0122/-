020a07e3121c8e58f168338d5406bf40effb8909
hua0122 <1835591342@qq.com>
2018-12-12 19:09:56 +0800

    html

--- application/api/controller/Protect.php
+++ application/api/controller/Protect.php
@@ -629,8 +629,8 @@ class Protect extends Api
 
             foreach ($list as $k=>$v){
                 $i = 0;//给一个变量，每次循环它就定义成0，到达下标都从0开始
-                foreach ($v as $v1){
-                    if(!$list[$k]['time1']){
+                foreach ($v as $k1=>$v1){
+                    if($k1!= "time1"){
                         $list[$k][$i] = $v1;
                     }
 
