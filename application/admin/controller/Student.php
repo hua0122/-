7de62b71a0880bb89117081e196800c61c6f6afa
hua0122 <1835591342@qq.com>
2018-12-15 17:58:58 +0800

    html

--- application/admin/controller/Student.php
+++ application/admin/controller/Student.php
@@ -168,11 +168,11 @@ class Student extends Admin
 
         //班级信息展示
         $grade_s = db('Grade')
-            ->field('sent_grade.*,sent_area.school_id')
+            ->field('sent_grade.*,sent_area.school_id,sent_area.id')
             ->join('sent_area','sent_area.id=sent_grade.area_id','left')
             ->where(array("status"=>0,array("sent_area.school_id"=>$school_id)))->select();
         $grade_x = db('Grade')
-            ->field('sent_grade.*,sent_area.school_id')
+            ->field('sent_grade.*,sent_area.school_id,sent_area.id')
             ->join('sent_area','sent_area.id=sent_grade.area_id','left')
             ->where(array("status"=>1,array("sent_area.school_id"=>$school_id)))->select();
         $this->assign("grade_s",$grade_s);
