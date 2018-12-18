4e78f66b88d88d8283a6ba30374b9d6a135e7221
hua0122 <1835591342@qq.com>
2018-12-18 16:37:47 +0800

    html

--- application/api/controller/Activity.php
+++ application/api/controller/Activity.php
@@ -159,7 +159,7 @@ class Activity extends Api
 
             $total_fee = $amount * 100;
             if (!empty($total_fee) && $total_fee > 0 ) {
-                $unifiedOrderResult = $wx->unifiedorder($total_fee, $data['openid'], '活动预存', $data['sn'],$school_id);
+                $unifiedOrderResult = $wx->unifiedorder($total_fee, $tel, '活动预存', $data['sn'],$school_id);
                 //var_dump($unifiedOrderResult);
                 $timeStamp = intval(time() / 10);
                 $url = $_SERVER["HTTP_REFERER"];
