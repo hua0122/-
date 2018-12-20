81edd2699c554022e118e057657bb60a53ee7e16
hua0122 <1835591342@qq.com>
2018-12-20 16:38:41 +0800

    html

--- l_wx/weixin.php
+++ l_wx/weixin.php
@@ -131,8 +131,8 @@ class Weixin_class {
 		$res = substr($res,1);
 		$res = "{" . $res . "}";
 		$res = json_decode($res);
-		var_dump($res);
-		exit;
+		//var_dump($res);
+		//exit;
 		return $res;
 	}
 
