185a365d8eb032f6a1cd8232bb2312863f0f1658
hua0122 <1835591342@qq.com>
2018-12-20 15:45:22 +0800

    html

--- l_wx/getwxinfo.php
+++ l_wx/getwxinfo.php
@@ -200,6 +200,8 @@ switch ($method) {//获取code
                 exit();
                 break;
             case 'djjxhd_api':
+                var_dump($data);
+                exit;
                 $url = "/api/user/getwxinfo_djjxhd?data=".$data;
                 header("Location:" . $url);
                 exit();
