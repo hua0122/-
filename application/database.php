a1f6ab0695ebc13359778ad68910bf81823497fe
hua0122 <1835591342@qq.com>
2018-12-24 15:10:34 +0800

    html

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return array(
 	// 服务器地址
 	'hostname'    => '127.0.0.1',
 	// 数据库名
-	'database'    => 'admin.yidainxueche.cn',
+	'database'    => 'admin.yidianxueche.cn',
 	// 数据库用户名
 	'username'    => 'root',
 	// 数据库密码
