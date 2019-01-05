4a15bb9cd35e437d94e0084294504b95ae1abf7d
hua0122 <1835591342@qq.com>
2019-01-05 17:23:56 +0800

    html

--- 11.php
+++ 11.php
@@ -1,6 +1,25 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/l_wx/config.php';
-$url = WEBURL."/api/protect/crontab";
-header("location:".$url);
+//include_once $_SERVER['DOCUMENT_ROOT'] . '/l_wx/config.php';
+//$url = WEBURL."/api/protect/crontab";
+//header("location:".$url);
 
+
+$con = new MySQLi("localhost","root","root","admin.yidianxueche.cn");
+//判断是否有错误
+
+if(mysqli_connect_error()){
+
+    echo "连接失败";
+
+    exit();
+
+}
+
+//写SQL语句
+
+$sql = "update sent_protect set status=2 where deactivation_time < unix_timestamp(now())";
+echo $sql;
+//执行SQL语句，返回结果集
+
+$result = $con->query($sql);
 ?>
